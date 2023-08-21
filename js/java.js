a4a1b1f12a7e779c325dada7fec345f379863ba4
Riad123456789 <riad80717@gmail.com>
2023-08-21 16:15:55 +0600

    my all commit

--- js/java.js
+++ js/java.js
@@ -1,5 +1,12 @@
 let total= 0;
 
+
+const submit =document.getElementById('submit')
+submit.disabled = true
+
+const secondSubmit =document.getElementById('secondSubmit')
+secondSubmit.disabled=true
+
 function handleClick(target){
     const itemsContainer =document.getElementById('items')
     const itemName =target.childNodes[3].childNodes[3].innerText;
@@ -12,10 +19,40 @@ function handleClick(target){
    total = parseFloat(total)+ parseFloat(price);
    const totalPrice= total.toFixed(2);
    document.getElementById('total').innerText =totalPrice;
+   document.getElementById('priceTotal').innerText=totalPrice;
 
-   
+if(totalPrice >= 0){
+    secondSubmit.disabled=false
 }
 
+if(totalPrice >= 200 ){
+    submit.disabled = false
+}
+
+}
+
+
+function apply(target){
+    const value =target.parentNode.childNodes[1].value;
+const code =document.getElementById('code').innerText
+
+
+    if(value ===code){
+        const p = total *20 /100
+        const fixed =p.toFixed(2)
+        const discountTotal= document.getElementById("discount").innerText=fixed
+      
+        const allTotal = total-discountTotal
+        const fixedAllTotal =allTotal.toFixed(2);
+      
+        document.getElementById('priceTotal').innerText=fixedAllTotal;
+
+    }else{
+        alert('invalid')
+    }
+
+}
+    
 
 document.getElementById('home').addEventListener('click',function(){
     window.location.href ="index.html";
